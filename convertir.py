77149a5952852d55e009f64849e80089b8b9612e
cylelectron <106988752+cylelectron@users.noreply.github.com>
2024-10-24 16:12:02 +0200

    Update convertir.py

--- convertir.py
+++ convertir.py
@@ -1,1 +1,8 @@
+import pyreadstat
+
+# Cargar el archivo DTA desde el repositorio
+df, meta = pyreadstat.read_dta('255LOCIN.DTA')
+
+# Guardar el archivo convertido
+df.to_stata('255LOCIN_converted.dta', version=117)
 
